5f2df2733053b76650b36fc35e1469bb0dc64a65
SeaHOH <Seahoh@Gmail.com>
2023-10-11 15:15:59 +0800

    debug

--- source/_memimporter.h
+++ source/_memimporter.h
@@ -34,7 +34,7 @@ EM_JS(PyObject*, _PyImport_InitFunc_TrampolineCall, (PyModInitFunction func), {
 #endif
 
 #if (PY_VERSION_HEX >= 0x030C0000)
-_PyRuntimeState *_My_PyRuntime = NULL;
+_PyRuntimeState *_My_PyRuntime;
 #define PKGCONTEXT ((*_My_PyRuntime).imports.pkgcontext)
 //const char *pkgcontext = "";
 //#define PKGCONTEXT pkgcontext
